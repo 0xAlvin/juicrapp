75e99f8d0aa056637e86f9c17cf8307d258ba8cf
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-20 16:08:09 +0300

    add: juice dao with access functions

---
+++ data/local/src/main/java/com/example/juicr/dao/JuiceDao.kt
@@ -0,0 +1,36 @@
+package com.example.juicr.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.juicr.entities.JuiceEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface JuiceDao{
+    @Insert
+    suspend fun addJuice(juice: JuiceEntity)
+
+    @Update
+    suspend fun updateJuice(juice: JuiceEntity): Int
+
+    @Delete
+    suspend fun deleteJuice(juice: JuiceEntity): Int
+
+    @Query("SELECT * FROM juices")
+    suspend fun getAll(): Flow<List<JuiceEntity>>
+
+    @Query("SELECT * FROM juices WHERE id = :id LIMIT 1")
+    suspend fun getById(id: Long): JuiceEntity
+
+    @Query("SELECT * FROM juices WHERE isFavourite = TRUE")
+    suspend fun getFavourites(): Flow<List<JuiceEntity>>
+
+    @Query("SELECT * FROM juices WHERE name LIKE :text")
+    suspend fun search(text: String): Flow<List<JuiceEntity>>
+
+    @Query("SELECT * FROM juices ORDER BY name DESC")
+    suspend fun sortByRating(): Flow<List<JuiceEntity>>
+}
