b9333cc57d9abf373ad67d9da4e611de9e7b2c72
Alvin <alvinvaati@gmail.com>
2026-01-22 12:42:17 +0300

    add: type converters for ingredients and dates

--- data/local/src/main/java/com/example/juicr/Database.kt
+++ data/local/src/main/java/com/example/juicr/Database.kt
@@ -2,6 +2,9 @@ package com.example.juicr
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.juicr.converters.DateConverter
+import com.example.juicr.converters.IngredientsConverter
 import com.example.juicr.dao.IJuiceDao
 import com.example.juicr.dao.IPrepHistoryDao
 import com.example.juicr.entities.JuiceEntity
@@ -10,6 +13,10 @@ import com.example.juicr.entities.PreparationHistoryEntity
 @Database(entities = [
     JuiceEntity::class,
     PreparationHistoryEntity::class ], version = 1)
+@TypeConverters(
+    IngredientsConverter::class,
+    DateConverter::class
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun juiceDao(): IJuiceDao
     abstract fun preparationHistoryDao(): IPrepHistoryDao
