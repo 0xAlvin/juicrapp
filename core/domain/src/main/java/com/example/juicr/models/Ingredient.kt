1a2a0c44b818905f30b1275e4340c4203c4921cb
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-23 10:45:03 +0300

    refactor: moved to models in domain

--- core/domain/src/main/java/com/example/juicr/models/Ingredient.kt
+++ core/domain/src/main/java/com/example/juicr/models/Ingredient.kt
@@ -1,4 +1,4 @@
-package com.example.domain.models
+package com.example.juicr.models
 data class Ingredient(
     val id: Long,
     val name: String,
--- core/domain/src/main/java/com/example/juicr/models/Juice.kt
+++ core/domain/src/main/java/com/example/juicr/models/Juice.kt
@@ -1,4 +1,4 @@
-package com.example.domain.models
+package com.example.juicr.models
 
 data class Juice(
     val id: String,
