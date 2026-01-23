9f8a10f659ae311ae46b457710e65d6f3085b2b0
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-23 10:40:59 +0300

    add hilt dependency

--- data/repositories/build.gradle.kts
+++ data/repositories/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -28,4 +30,7 @@ dependencies {
     implementation(project(":data:local"))
 
     implementation(libs.androidx.core.ktx)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 }
