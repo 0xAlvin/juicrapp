09129d75180b32396fb610065081a3e7a1c938e3
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-23 10:43:59 +0300

    update: use flows properly for observable functions, modify search query

--- data/local/src/main/java/com/example/juicr/dao/IJuiceDao.kt
+++ data/local/src/main/java/com/example/juicr/dao/IJuiceDao.kt
@@ -9,29 +9,41 @@ import androidx.room.Update
 import com.example.juicr.entities.JuiceEntity
 import kotlinx.coroutines.flow.Flow
 
+
 @Dao
-interface IJuiceDao{
+interface IJuiceDao {
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addJuice(juice: JuiceEntity)
+    suspend fun addJuice(juice: JuiceEntity): Long
 
     @Update
     suspend fun updateJuice(juice: JuiceEntity): Int
 
-    @Delete
-    suspend fun deleteJuice(juice: JuiceEntity): Int
+    @Query("DELETE FROM juices WHERE id = :id")
+    suspend fun deleteJuice(id: Long): Int
+
+    @Query("UPDATE juices SET isFavourite = NOT isFavourite WHERE id = :id")
+    suspend fun toggleFavorite(id: Long): Int
 
     @Query("SELECT * FROM juices")
-    suspend fun getAll(): Flow<List<JuiceEntity>>
+    fun getAll(): Flow<List<JuiceEntity>>
 
     @Query("SELECT * FROM juices WHERE id = :id LIMIT 1")
-    suspend fun getById(id: Long): JuiceEntity
+    fun getById(id: Long): Flow<JuiceEntity?>
 
-    @Query("SELECT * FROM juices WHERE isFavourite = TRUE")
-    suspend fun getFavourites(): Flow<List<JuiceEntity>>
+    @Query("SELECT * FROM juices WHERE isFavourite = 1")
+    fun getFavourites(): Flow<List<JuiceEntity>>
 
-    @Query("SELECT * FROM juices WHERE name LIKE :text")
-    suspend fun search(text: String): Flow<List<JuiceEntity>>
+    @Query("""
+        SELECT * FROM juices
+        WHERE name LIKE '%' || :text || '%' COLLATE NOCASE
+    """)
+    fun search(text: String): Flow<List<JuiceEntity>>
 
     @Query("SELECT * FROM juices ORDER BY name DESC")
-    suspend fun sortByRating(): Flow<List<JuiceEntity>>
+    fun sortByNameDesc(): Flow<List<JuiceEntity>>
+
+    @Query("SELECT * FROM juices ORDER BY rating DESC")
+    fun sortByRating(): Flow<List<JuiceEntity>>
 }
+
