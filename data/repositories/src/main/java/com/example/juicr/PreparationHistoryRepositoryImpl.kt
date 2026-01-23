521d56f59433be3786b5f152072aec0b8e32dac5
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-23 11:15:19 +0300

    add: PreparationHistoryRepository implemetation

---
+++ data/repositories/src/main/java/com/example/juicr/PreparationHistoryRepositoryImpl.kt
@@ -0,0 +1,31 @@
+package com.example.juicr
+
+import com.example.juicr.dao.IPrepHistoryDao
+import com.example.juicr.mappers.toDomain
+import com.example.juicr.mappers.toEntity
+import com.example.juicr.models.PreparationHistory
+import com.example.juicr.repositories.IPreparationHistory
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class PreparationHistoryRepositoryImpl @Inject constructor(
+    private val prepHistoryDao: IPrepHistoryDao
+): IPreparationHistory {
+    override suspend fun addHistory(prepHistory: PreparationHistory): Int {
+        return prepHistoryDao.addHistory(prepHistory.toEntity())
+    }
+
+    override fun getHistoryByJuiceId(id: Long): Flow<List<PreparationHistory>> {
+        return prepHistoryDao.getHistoryByJuiceId(id).map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+
+    override fun getRecentHistory(limit: Int?): Flow<List<PreparationHistory>> {
+        return prepHistoryDao.getRecentHistory(limit).map { entities ->
+            entities.map { it -> it.toDomain() }
+        }
+    }
+
+}
