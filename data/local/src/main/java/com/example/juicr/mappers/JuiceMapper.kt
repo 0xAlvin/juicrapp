bbd5d030d6d43cb4fcb471e1429708f12330efab
Alvin <alvinvaati@gmail.com>
2026-01-23 06:51:18 +0300

    add: implement JuiceMapper with conversion functions for Juice and JuiceEntity

---
+++ data/local/src/main/java/com/example/juicr/mappers/JuiceMapper.kt
@@ -0,0 +1,40 @@
+package com.example.juicr.mappers
+
+import com.example.domain.models.Ingredient
+import com.example.domain.models.Juice
+import com.example.juicr.entities.IngredientEntity
+import com.example.juicr.entities.JuiceEntity
+import kotlin.String
+
+fun JuiceEntity.toDomain(): Juice {
+    return Juice(
+        id = id,
+        name = name,
+        description = description,
+        color = color,
+        rating = rating,
+        dateAdded = dateAdded,
+        prepTime = prepTime,
+        ingredients = ingredients.map { (id,name, category, imgRes) ->
+            Ingredient(id,name,category,imgRes) },
+        timesPrepared = timesPrepared,
+        isFavourite = isFavourite
+    )
+}
+
+fun Juice.toEntity(): JuiceEntity {
+    return JuiceEntity(
+        id = id,
+        name = name,
+        description = description,
+        color = color,
+        rating = rating,
+        dateAdded = dateAdded,
+        prepTime = prepTime,
+        ingredients = ingredients.map { (id, name, category, imgRes) ->
+            IngredientEntity(id,name, category, imgRes)
+        },
+        timesPrepared = timesPrepared,
+        isFavourite = isFavourite
+    )
+}
