8cf41754644e9a9f130cb5a3b2a2f5836d890933
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-26 14:50:27 +0300

    homescreen

--- feature/home/src/main/java/com/example/juicr/ui/HomeScreen.kt
+++ feature/home/src/main/java/com/example/juicr/ui/HomeScreen.kt
@@ -1,13 +1,59 @@
 package com.example.juicr.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.juicr.feature.home.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    viewModel: HomeViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = hiltViewModel()
 ){
+    val scrollBehaviour = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.fillMaxWidth(),
+                title = { Text(stringResource(R.string.homescreen_topbar_text)) },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = stringResource(R.string.searchbar_icon_content_description)
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehaviour
+            )
+        }
+    ) { inner ->
+        Column(
+            modifier = Modifier.padding(inner)
+        ) { }
+    }
 
+}
+
+@Preview
+@Composable
+fun HomePreview() {
+    HomeScreen()
 }
