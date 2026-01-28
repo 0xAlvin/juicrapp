243ff00d84f4b8cdd6803732235942e61bbf1a41
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-28 15:34:02 +0300

    feat:bottom navigation bar

---
+++ core/presentation/src/main/java/com/example/presentation/components/BottomNavBar.kt
@@ -0,0 +1,9 @@
+package com.example.presentation.components
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun BottomNavBar(
+    modifier: Modifier = Modifier
+) {}
