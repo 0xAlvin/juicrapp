65184f819919fdb70d6c5827a1022b93aca93377
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-28 15:31:48 +0300

    add: search usecase

---
+++ core/domain/src/main/java/com/example/juicr/usecases/SearchJuiceUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.juicr.usecases
+
+import com.example.juicr.models.Juice
+import com.example.juicr.repositories.IJuiceRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchJuiceUseCase @Inject constructor(
+    private val juiceRepository: IJuiceRepository
+) {
+    operator fun invoke(query: String): Flow<List<Juice>>{
+        return juiceRepository.search(query)
+    }
+}
