e2427674405c524c2b9048b9393fc3c4b08a000b
Alvin.Vaati <alvinvaati@gmail.com>
2026-01-28 16:10:11 +0300

    fix: serialization dep

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 
     implementation(libs.kotlinx.serialization.json)
 
+
     // AndroidX
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
